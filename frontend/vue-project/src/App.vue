099e75501c49a5ee9e56f1e4151bb0e198293bd6
김태범 <ktb5057@gmail.com>
2024-01-17 15:46:12 +0900

    임시 라우터 버튼 셋팅

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,13 +1,46 @@
 <template>
-  <div>
-    
+  <div id="main-body">
+    <header>
+      <div class="wrapper">
+        <h2>Router For Development</h2>
+        <nav>
+          <RouterLink to="/">Main</RouterLink>
+          <RouterLink to="/competition">Competiton</RouterLink>
+          <RouterLink to="/solo">Solo</RouterLink>
+          <RouterLink to="/feed">Feed</RouterLink>
+          <RouterLink to="/mypage">MyPage</RouterLink>
+          <RouterLink to="/login">Login</RouterLink>
+          <RouterLink to="/signup">SignUp</RouterLink>
+        </nav>
+      </div>
+    </header>
+  <br>
+    <RouterView />
   </div>
 </template>
 
 <script setup>
-
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <style scoped>
+#main-body{
+  border: 2px solid gainsboro;
 
+  max-width: 1000px;
+  height: 95vh;
+  margin: auto;
+
+  display: flex;
+  flex-direction: column;
+}
+
+a {
+  border: 1px solid green;
+  margin: 5px;
+  padding-inline: 10px;
+}
+nav {
+  margin-top: 10px;
+}
 </style>
