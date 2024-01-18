b0f8537a4f8f255e038009d10da23bb61599082d
“hwasok” <hwasuk528@naver.com>
2024-01-18 12:48:36 +0900

    [FE]SIGNUP&LOGIN

--- frontend/vue-project/src/stores/signup.js
+++ frontend/vue-project/src/stores/signup.js
@@ -6,90 +6,110 @@ import axios from 'axios';
 
 
 export const useSignupStore = defineStore('signup', () => {
-
+    const signup = ref(null);
     const ssafyurl = 'https://project.ssafy.com/ssafy/api/auth/signin';
     const URL = 'http://localhost:8080/user/signup';
-    const isssafy = function(ssafy) {
+    const isssafy = function (ssafy) {
         console.log('되고있나');
-    
-    
+
         return new Promise((resolve, reject) => {
             axios
                 .post(ssafyurl, ssafy)
                 .then((response) => {
-    
+
                     resolve(response);
                 })
                 .catch((e) => {
                     console.log(e)
                     reject(e);
-    
+
                 });
         }
-    
+
         );
     }
-    const isid = function(IdData) {
+    const isid = function (IdData) {
         console.log('되고있나')
-    
-    
+
+
         return new Promise((resolve, reject) => {
             axios
                 .get(`${URL}/check-id/${IdData}`)
                 .then((response) => {
-    
+
                     resolve(response);
                 })
                 .catch((e) => {
                     console.log(e)
                     reject(e);
-    
+
                 });
         }
-    
+
         );
     }
-    const isnickname = function(NicknameData) {
+    const isnickname = function (NicknameData) {
         console.log('되고있나')
-    
-    
+
+
         return new Promise((resolve, reject) => {
             axios
                 .get(`${URL}/check-nick/${NicknameData}`)
                 .then((response) => {
-    
+
                     resolve(response);
                 })
                 .catch((e) => {
                     console.log(e)
                     reject(e);
-    
+
                 });
         }
-    
+
         );
     }
-    const isstudentnum = function(StudentnumData) {
+    const isstudentnum = function (StudentnumData) {
         console.log('되고있나')
-    
-    
+
+
         return new Promise((resolve, reject) => {
             axios
                 .get(`${URL}/check-student-num/${StudentnumData}`)
                 .then((response) => {
-    
+
                     resolve(response);
                 })
                 .catch((e) => {
                     console.log(e)
                     reject(e);
-    
+
                 });
         }
-    
+
         );
     }
-    return {isid, isnickname, isssafy, isstudentnum}
+    const submitNewUser = function (newUser) {
+        console.log('되고있나')
+
+
+        return new Promise((resolve, reject) => {
+            axios
+                .post(URL, newUser)
+                .then((response) => {
+
+                    resolve(response);
+                    router.push('/');
+                })
+                .catch((e) => {
+                    console.log(e)
+                    reject(e);
+
+                });
+        }
+
+        );
+    }
+    return { signup, isid, isnickname, isssafy, isstudentnum, submitNewUser }
 })
 
 
