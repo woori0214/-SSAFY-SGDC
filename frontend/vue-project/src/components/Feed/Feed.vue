1e4c6863dc00ab58610700e5e43e931b2d822bd1
김태범 <ktb5057@gmail.com>
2024-01-18 16:50:03 +0900

    피드 게시판 피드 프로필 신고하기 버튼 추가

--- frontend/vue-project/src/components/Feed/Feed.vue
+++ frontend/vue-project/src/components/Feed/Feed.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="feed_frame">
-    <div class="feed_user_name">{{ userName }}</div>
+    <div class="feed_profile">
+      <div class="feed_user_name">{{ userName }}</div>
+      <button>신고하기</button>
+    </div>
     <div class="feed_content">{{ content }}</div>
     <div class="feed_image_frame" @click="routeDetailFeed()">
       <img :src="feedImage" class="feed_image" />
     </div>
     <div class="feed_footbar">
-      <div class="feed_heart_cnt">{{ heartCnt }}</div>
-      <div class="feed_view_cnt">{{ viewCnt }}</div>
+      <div class="feed_heart_cnt">좋아요 {{ heartCnt }}</div>
+      <div class="feed_view_cnt">조회수 {{ viewCnt }}</div>
     </div>
   </div>
 </template>
@@ -49,30 +52,42 @@ export default {
 
 <style>
 .feed_frame {
-  border: 2px solid skyblue;
+  /* border: 2px solid skyblue; */
+}
+.feed_profile{
+    /* border: 2px solid skyblue; */
+    display: flex;
+    justify-content: space-between;
 }
 .feed_user_name {
   /* border: 2px solid purple; */
+  display: flex;
+  align-items: center;
 }
 .feed_content {
   /* border: 2px solid purple; */
 }
 .feed_image_frame {
-  /* border: 2px solid purple; */
+  border: 2px solid whitesmoke;
+  border-radius: 30px;
+  background-color: azure;
   display: flex;
   justify-content: center;
+  margin: 10px;
+  padding: 20px;
 }
 .feed_image {
   /* border: 2px solid blue; */
-  width: 150px;
+  max-width: 300px;
+  max-height: 500px;
 }
 .feed_footbar {
   /* border: 2px solid purple; */
   display: flex;
+  justify-content: space-between;
 }
 .feed_heart_cnt {
   /* border: 2px solid blue; */
-  margin-right: 10px;
 }
 .feed_view_cnt {
   /* border: 2px solid blue; */
