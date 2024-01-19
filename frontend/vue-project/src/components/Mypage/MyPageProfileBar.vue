4304d23e16daec33b5b4d16e3a3befcf220d0936
hon1770 <hon1770@naver.com>
2024-01-19 14:04:12 +0900

    [FE]mypageprofile

--- frontend/vue-project/src/components/Mypage/MyPageProfileBar.vue
+++ frontend/vue-project/src/components/Mypage/MyPageProfileBar.vue
@@ -1,13 +1,119 @@
 <template>
     <div>
+        <div class="top">
+            <h2>{{ user.nickname }} 님</h2>
+            <div>
+                <img v-for="complain in user.complain_cnt" :key="complain" src="@/assets/pinno.png" alt="pinno"
+                    class="complain_img">
+            </div>
+        </div>
+        <div class="middle">
+            <div class="middle1">
+                <div class="user_img_badge">
+                    <img :src="user.img" alt="use_img" class="user_img">
+                    <p>{{ user.badge_id }}</p>
+                </div>
+                <div>
+                    <button class="friend_btn">도전장 보내기</button>
+                    <button class="friend_btn">쌀로우 신청</button>
+                </div>
+            </div>
+            <div class="follow_cnt">
+                <div class="follow_div">
+                    <h1>{{ user.ssallower_friends.length }}</h1>
+                    <h3>쌀로워 수</h3>
+                </div>
+                <div class="follow_div">
+                    <h1>{{ user.ssallowing_friends.length }}</h1>
+                    <h3>쌀로잉 수</h3>
+                </div>
+            </div>
 
+        </div>
     </div>
 </template>
 
 <script setup>
+const user = {
+    nickname: '우리다',
+    img: './src/assets/image1.png',
+    ssallower_friends: [
+        {
+            id: 1,
+            name: '화석'
+        },
+        {
+            id: 2,
+            name: '태범'
+        },
+        {
+            id: 3,
+            name: '지은'
+        },
+    ],
+    ssallowing_friends: [
+        {
+            id: 1,
+            name: '현춘'
+        },
+        {
+            id: 2,
+            name: '수안'
+        },
+    ],
+    complain_cnt: 2,
+    badge_id: '분조장'
 
+}
 </script>
 
 <style scoped>
+.top {
+    display: flex;
+    justify-content: space-between;
+    /* 중앙으로 맞춤 */
+    align-items: center;
+    margin: 0 10px;
+}
 
+.complain_img {
+    border: 1px black solid;
+    border-radius: 100px;
+    background-color: aliceblue;
+    width: 30px;
+    height: 30px;
+    margin: 2px;
+}
+
+.friend_btn{
+    margin: 10px;
+}
+.user_img_badge {
+    display: flex;
+    align-items: center;
+}
+
+.user_img {
+    border: 1px black solid;
+    border-radius: 500px;
+    widows: 130px;
+    height: 130px;
+    margin: 10px;
+}
+
+.middle {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.follow_cnt {
+    display: flex;
+    margin-right: 30px;
+    text-align: center;
+}
+
+.follow_div {
+    margin: 0 50px;
+}
 </style>
--- frontend/vue-project/src/views/MyPageView.vue
+++ frontend/vue-project/src/views/MyPageView.vue
@@ -1,13 +1,94 @@
 <template>
-    <div>
+    <div class="mypageview_body">
         <h1>프로필페이지</h1>
+        <div class="profile_body"> 
+            <MyPageProfileBar />
+        </div>
+        <div class="mycategory_body">
+            <MyPageCategory />
+        </div>
+        <div class="compet_body">
+            <MyPageCompetitionRecord />
+        </div>
+        <div class="solo_body">
+            <MyPageSoloRecord />
+        </div>
+        <div class="analysis_body">
+            <MyPageAnalysis />
+        </div>
+        <div class="challenge_body">
+            <MyPageChallengeBoard />
+        </div>
+        <div class="badge_body">
+            <MyPageBadgeList />
+        </div>
+        <div class="ssallow_body">
+            <MyPageSsallow />
+        </div>
     </div>
 </template>
 
 <script setup>
+import MyPageAnalysis from '@/components/Mypage/MyPageAnalysis.vue';
+import MyPageBadgeList from '@/components/Mypage/MyPageBadgeList.vue';
+import MyPageCategory from '@/components/Mypage/MyPageCategory.vue';
+import MyPageChallengeBoard from '@/components/Mypage/MyPageChallengeBoard.vue';
+import MyPageCompetitionRecord from '@/components/Mypage/MyPageCompetitionRecord.vue';
+import MyPageProfileBar from '@/components/Mypage/MyPageProfileBar.vue';
+import MyPageSoloRecord from '@/components/Mypage/MyPageSoloRecord.vue';
+import MyPageSsallow from '@/components/Mypage/MyPageSsallow.vue';
+
 
 </script>
 
 <style scoped>
+.mypageview_body {
+    border: 2px solid red;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+}
+
+.profile_body {
+    border: 1px solid rgb(51, 0, 255);
+    
+}
+
+.mycategory_body {
+    border: 1px solid rgb(0, 255, 76);
+    
+}
+
+.compet_body {
+    border: 1px solid rgb(255, 0, 132);
+    ;
+}
+
+.solo_body {
+    border: 1px solid rgb(0, 238, 255);
+    
+}
+
+.analysis_body {
+    border: 1px solid rgb(244, 156, 189);
+    
+}
+
+.challenge_body {
+    border: 1px solid rgb(247, 0, 255);
+    
+}
+
+.badge_body {
+    border: 1px solid rgb(210, 247, 0);
+    
+}
+
+.ssallow_body {
+    border: 1px solid rgb(0, 70, 22);
+    
+}
+
+
 
 </style>
