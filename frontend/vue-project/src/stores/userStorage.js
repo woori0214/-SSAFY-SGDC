3ff9b1830cb2af639d5542d879ef90a29b2c8388
김태범 <ktb5057@gmail.com>
2024-01-24 09:16:09 +0900

    유저 토큰을 통한 사용자 인증 함수 추가(아직 API는 없음)

--- frontend/vue-project/src/stores/userStorage.js
+++ frontend/vue-project/src/stores/userStorage.js
@@ -6,12 +6,6 @@ export const useUserStorageStore = defineStore('userStorage', () => {
     localStorage.setItem(item, data);
   }
 
-  const setUserInformation = function (data) {
-    localStorage.setItem("user_id", data.user_id);
-    localStorage.setItem("user_name", data.user_name);
-    localStorage.setItem("token", data.token);
-  }
-
   const getStorage = function (item) {
     return localStorage.getItem(item);
   }
@@ -24,6 +18,12 @@ export const useUserStorageStore = defineStore('userStorage', () => {
     localStorage.removeItem(item);
   }
 
+  const setUserInformation = function (data) {
+    localStorage.setItem("user_id", data.user_id);
+    localStorage.setItem("user_name", data.user_name);
+    localStorage.setItem("token", data.token);
+  }
+
   const getUserInformation = function () {
     const user_id = localStorage.getItem("user_id");
     const user_name = localStorage.getItem("user_name");
@@ -34,6 +34,17 @@ export const useUserStorageStore = defineStore('userStorage', () => {
       user_name: user_name || null,
       token: token || null,
     }
+  }
+
+  const userAuthentication = function () {
+    const user_id = localStorage.getItem("user_id");
+    const token = localStorage.getItem("token");
+
+    // 유저 로그인 토큰 인증 API 들어갈 자리
+    // API에 user_id와 token를 같이 보내고 
+    // 해당 user_id의 현재 로그인 token이 일치하는지 확인
+    // 해당 정보가 인증되면 return true
+    // 아니면 return false
   }
 
   return {
