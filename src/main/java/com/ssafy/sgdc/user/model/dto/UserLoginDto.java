b86af7174bd2f9c177c9ee59f77dc117ca70b588
DESKTOP-LL832F4\SSAFY <woori50300@gmail.com>
2024-01-25 14:46:48 +0900

    [ADD] : 로그인 Dto

--- src/main/java/com/ssafy/sgdc/user/model/dto/UserLoginDto.java
+++ src/main/java/com/ssafy/sgdc/user/model/dto/UserLoginDto.java
@@ -1,2 +1,15 @@
-package com.ssafy.sgdc.user.model.dto;public class UserLogin {
+package com.ssafy.sgdc.user.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDto {
+
+    private String loginId;
+    private String userPassword;
+
 }
