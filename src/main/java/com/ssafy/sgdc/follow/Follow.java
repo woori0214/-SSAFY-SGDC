bbeb1b5faf52fea078ac13289abada3fe2c21e80
Yoosuean <awetumnn@gmail.com>
2024-01-30 14:10:39 +0900

    [FEAT] 팔로우 기능 추가 - entity, controller, repo, service - 팔로우, 언팔로우 - 팔로워 조회, 팔로잉 조회

---
+++ src/main/java/com/ssafy/sgdc/follow/Follow.java
@@ -0,0 +1,36 @@
+package com.ssafy.sgdc.follow;
+import com.ssafy.sgdc.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(
+        name = "follow",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "follow_uk",
+                        columnNames = {"user_id", "following_id"}
+                )
+        }
+)
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    @Column(name = "friend_id")
+    private long friendId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id")
+    private User followingId;
+
+}
