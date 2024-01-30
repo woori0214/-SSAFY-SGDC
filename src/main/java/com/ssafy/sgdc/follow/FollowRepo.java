fb4fe220d3441836922ce8c507022e598dc1bf94
Yoosuean <awetumnn@gmail.com>
2024-01-30 14:10:39 +0900

    [FEAT] 팔로우 기능 추가 - entity, controller, repo, service - 팔로우, 언팔로우 - 팔로워 조회, 팔로잉 조회

---
+++ src/main/java/com/ssafy/sgdc/follow/FollowRepo.java
@@ -0,0 +1,34 @@
+package com.ssafy.sgdc.follow;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FollowRepo extends JpaRepository<Follow, Long> {
+
+
+    // 팔로잉
+    @Modifying
+    @Query(value="INSERT INTO follow(user_id,following_id) VALUES(:userId, :followingId)", nativeQuery=true)
+    void mFollow(@Param("userId") Long userId, @Param("followingId") Long followingId);
+
+    // 언팔로잉
+    @Modifying
+    @Query(value="DELETE FROM follow  WHERE user_id = :userId AND following_id = :followingId", nativeQuery=true)
+    void mUnFollow(@Param("userId") Long userId, @Param("followingId") Long followingId);
+
+    // 팔로워 조회
+    @Query(value = "SELECT * from follow f  where f.following_id = :userId", nativeQuery = true)
+    List<Follow> getListByFromUserId(Long userId);
+
+    // 팔로잉 조회
+    @Query(value = "SELECT * from follow f  where f.user_id = :followingId", nativeQuery = true)
+    List<Follow> getListByToUserId(Long followingId);
+
+
+}
