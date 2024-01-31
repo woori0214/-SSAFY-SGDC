93765fda80f483b582720b2cd33ecb26699e1a1a
Yoosuean <awetumnn@gmail.com>
2024-01-31 14:43:39 +0900

    [ADD] 팔로잉, 팔로워 수 조회 API 추가 - swagger 어노테이션 springdoc 버전으로 변경

--- src/main/java/com/ssafy/sgdc/follow/FollowController.java
+++ src/main/java/com/ssafy/sgdc/follow/FollowController.java
@@ -1,28 +1,37 @@
 package com.ssafy.sgdc.follow;
 
 import com.ssafy.sgdc.follow.dto.CMRespDto;
+import com.ssafy.sgdc.follow.dto.FollowCountDto;
 import com.ssafy.sgdc.follow.dto.FollowerListResponseDto;
 import com.ssafy.sgdc.follow.dto.FollowingListResponseDto;
+import com.ssafy.sgdc.user.User;
 import com.ssafy.sgdc.util.response.GeneralResponse;
-import io.swagger.annotations.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+
 @ApiResponses({
-        @ApiResponse(code = 200, message = "OK"),
-        @ApiResponse(code = 201, message = "CREATED"),
-        @ApiResponse(code = 400, message = "BAD REQUEST"),
-        @ApiResponse(code = 500, message = "SERVER ERROR")
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "201", description = "CREATED"),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+        @ApiResponse(responseCode = "500", description = "SERVER ERROR")
 })
-@Api(tags = {"팔로우 정보를 제공하는 Controller"})
+@Tag(name="팔로우 API", description = "팔로우 정보를 제공하는 Controller")
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin("*")
-@RequestMapping("/following")
+@RequestMapping("api/v1/follow")
 class FollowController {
 
     private final FollowService followService;
@@ -30,12 +39,12 @@ class FollowController {
     /**
      * 팔로우 걸기
      */
-    @ApiOperation(value = "팔로우 성공", notes="다른 유저에게 팔로우를 합니다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "toUserId", dataType = "Long", value = "팔로우 받는 유저 PK", paramType = "path"),
-            @ApiImplicitParam(name = "fromUserId", dataType = "Long", value = "팔로우 하는 유저 PK", paramType = "path")
+    @Operation(summary = "팔로우 성공", description="다른 유저에게 팔로우를 합니다.")
+    @Parameters({
+            @Parameter(name = "toUserId", schema=@Schema(implementation = Long.class), description = "팔로우 받는 유저 PK", in = ParameterIn.PATH),
+            @Parameter(name = "fromUserId",  schema=@Schema(implementation = Long.class), description = "팔로우 하는 유저 PK", in = ParameterIn.PATH)
     })
-    @PostMapping("/follow/{toUserId}/{fromUserId}")
+    @PostMapping("/{toUserId}/{fromUserId}")
     public ResponseEntity<?> follow(@PathVariable Long toUserId, @PathVariable Long fromUserId) {
         followService.follow(toUserId, fromUserId);
         return new ResponseEntity<>(GeneralResponse.builder()
@@ -47,12 +56,12 @@ class FollowController {
     /**
      * 팔로우 취소
      */
-    @ApiOperation(value = "팔로우 취소", notes="다른 유저에게 건 팔로우를 취소합니다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "toUserId", dataType = "Long", value = "팔로우 받는 유저 PK", paramType = "path"),
-            @ApiImplicitParam(name = "fromUserId", dataType = "Long", value = "팔로우 하는 유저 PK", paramType = "path")
+    @Operation(summary= "팔로우 취소", description="다른 유저에게 건 팔로우를 취소합니다.")
+    @Parameters({
+            @Parameter(name = "toUserId", schema = @Schema(implementation = Long.class), description = "팔로우 받는 유저 PK", in = ParameterIn.PATH),
+            @Parameter(name = "fromUserId", schema = @Schema(implementation = Long.class), description = "팔로우 하는 유저 PK", in = ParameterIn.PATH)
     })
-    @DeleteMapping("/follow/{toUserId}/{fromUserId}")
+    @DeleteMapping("/{toUserId}/{fromUserId}")
     public ResponseEntity<?> unfollow(@PathVariable Long toUserId, @PathVariable Long fromUserId){
         followService.unFollow(toUserId, fromUserId);
         return new ResponseEntity<>(GeneralResponse.builder()
@@ -65,8 +74,8 @@ class FollowController {
     /**
      * 팔로워 리스트
      */
-    @ApiOperation(value = "팔로워 리스트", notes="팔로워 리스트를 확인합니다.")
-    @ApiImplicitParam(name = "toUserId", dataType = "Long", value = "팔로우 받는 유저 PK", paramType = "path")
+    @Operation(summary = "팔로워 리스트", description="팔로워 리스트를 확인합니다.")
+    @Parameter(name = "toUserId", schema = @Schema(implementation = Long.class), description = "팔로우 받는 유저 PK", in = ParameterIn.PATH)
     @GetMapping("/followerList/{toUserId}")
     public ResponseEntity<?> followerList(@PathVariable Long toUserId){
         List<FollowerListResponseDto> followerListResponseDtoList = followService.followerList(toUserId);
@@ -76,13 +85,25 @@ class FollowController {
     /**
      * 팔로잉 리스트
      */
-    @ApiOperation(value = "팔로잉 리스트", notes="팔로잉 리스트를 확인합니다.")
-    @ApiImplicitParam(name = "fromUserId", dataType = "Long", value = "팔로우 하는 유저 PK", paramType = "path")
+    @Operation(summary = "팔로잉 리스트", description="팔로잉 리스트를 확인합니다.")
+    @Parameter(name = "fromUserId", schema = @Schema(implementation = Long.class), description = "팔로우 하는 유저 PK", in = ParameterIn.PATH)
     @GetMapping("/followingList/{fromUserId}")
     public ResponseEntity<?>followingList(@PathVariable Long fromUserId){
         List<FollowingListResponseDto> followingListResponseDtoList = followService.followingList(fromUserId);
         return new ResponseEntity<>(new CMRespDto<>(200, "팔로잉리스트 조회", followingListResponseDtoList), HttpStatus.OK);
     }
 
+
+    /**
+     * 팔로잉 팔로워 수 조회
+     */
+    @Operation(summary = "팔로잉 팔로워 수", description="팔로잉 팔로워 수를 확입합니다.")
+    @Parameter(name = "fromUserId", schema = @Schema(implementation = Long.class), description = "팔로우 하는 유저 PK", in = ParameterIn.PATH)
+    @GetMapping("/followCount/{userId}")
+    public ResponseEntity<?> followCount(@PathVariable User userId){
+        Long following = followService.followingCount(userId);
+        Long follower = followService.followerCount(userId);
+        return new ResponseEntity<>(new CMRespDto<>(200, "팔로잉리스트 조회", new FollowCountDto(follower,following)), HttpStatus.OK);
+    }
 
 }
